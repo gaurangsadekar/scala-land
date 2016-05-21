fcb358d430a0ff766c2aa31ed85de1f096fa06ed
Gaurang Sadekar <gaurang.sadekar@gmail.com>
2016-05-21 19:44:24 +0530

    hw 1, 2/3

---
+++ recfun - week1/src/main/scala/recfun/Main.scala
@@ -0,0 +1,63 @@
+package recfun
+import common._
+
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(r: Int, n: Int): Int = {
+    // aux function computes nCr
+    @tailrec
+    def aux(n: Int, r: Int, num: Int, den: Int): Int = {
+       if (r == 0)
+         num / den
+       else
+         aux(n - 1, r - 1, num * n, den * r)
+    }
+    val col: Int = if (r < n - r) r else n - r
+    aux(n, col, 1, 1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def aux(stack: List[Char], chars: List[Char]): Boolean = {
+      // out of characters, then stack should be empty
+      if (chars.isEmpty)
+        stack.isEmpty
+      else {
+        val ch: Char = chars.head
+          // if open bracket, put it into the stack
+          if (ch == '(')
+            aux(ch :: stack, chars.tail)
+          // if closed bracket, pop one ( from stack
+          else if (ch == ')') {
+            if (!stack.isEmpty)
+              aux(stack.tail, chars.tail)
+            else false
+          }
+          else
+          // neither bracket, then go about business
+            aux(stack, chars.tail)
+      }
+    }
+    aux(List.empty, chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
