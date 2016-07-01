7c0a631fe03bb9f23fefe2f6b77f99081be2f017
Gaurang Sadekar <gaurang.sadekar@gmail.com>
2016-07-01 00:29:58 -0400

    some more cleanup

--- funsets - week2/src/main/scala/funsets/Main.scala
+++ funsets - week2/src/main/scala/funsets/Main.scala
@@ -5,3 +5,13 @@ object Main extends App {
   println(contains(singletonSet(1), 1))
   println(contains(singletonSet(2), 1))
 }
+
+/*
+    the anonymous function defined with the => operator
+    and methods defined using the def keyword are 2 completely different types
+    
+    this is why we have to use _ after partial def evaluations many times.
+    the compiler gives different toString type signatures to both of them
+    
+    => gives the FunctionN type and def gives the MethodN type
+*/
